f0828fc4c92db423e00053892e5edd7369e94b74
Mohamed Kaba <39755459+Momedy900@users.noreply.github.com>
2021-09-28 15:54:29 -0700

    Add files via upload

---
+++ milestokilogram.java
@@ -0,0 +1,30 @@
+package com.mohamedkaba;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static final double ML2KL = 1.60935;
+
+    public static void main(String[] args) {
+
+	Scanner scan = new Scanner (System.in);
+    double inputMiles = 0.0;
+    double resultKilogram = 0.0;
+
+        System.out.println("Miles To Kilogram");
+        System.out.print("How many litres? ");
+        inputMiles = scan.nextDouble();
+
+        resultKilogram =  inputMiles * ML2KL;
+
+        System.out.print    ("Converted: " + inputMiles + " kg" + " = ");
+        System.out.println  (resultKilogram + " kilogram");
+
+
+
+
+
+
+    }
+}
