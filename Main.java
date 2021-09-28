247f9d4dd162bb7db6599a1e2203deac22adeb6d
Mohamed Kaba <39755459+Momedy900@users.noreply.github.com>
2021-09-28 15:43:50 -0700

    Add files via upload
    Miles To kilogram

--- Main.java
+++ Main.java
@@ -3,31 +3,28 @@ package com.mohamedkaba;
 import java.util.Scanner;
 
 public class Main {
+
+    static final double ML2KL = 1.60935;
+
     public static void main(String[] args) {
-        // Name - Mohamed Kaba
-        // Student ID - T00690092
-        // Purpose of Program - Calculation in Java
-        // I did encounter some precision errors
-        Scanner scan = new Scanner (System.in);
-        System.out.println("Please enter two values");
 
-        float valueA, valueB = 0;
-        System.out.print("Value A: ");
-        valueA = scan.nextFloat();
-        System.out.print("Value B: ");
-        valueB = scan.nextFloat();
+	Scanner scan = new Scanner (System.in);
+    double inputMiles = 0.0;
+    double resultKilogram = 0.0;
 
-        float sum = valueA + valueB;
-        float difference = valueA - valueB;
-        float multiplication = valueA * valueB;
-        float quotient = valueA / valueB;
-        float remainder = valueA % valueB;
+        System.out.println("Miles To Kilogram");
+        System.out.print("How many litres? ");
+        inputMiles = scan.nextDouble();
 
-        System.out.println("Calculation for " + valueA + " and " + valueB);
-        System.out.println("\t" + valueA + " + " + valueB + " = " + sum);
-        System.out.println("\t" + valueA + " - " + valueB + " = " + difference);
-        System.out.println("\t" + valueA + " * " + valueB + " = " + multiplication);
-        System.out.println("\t" + valueA + " / " + valueB + " = " + quotient);
-        System.out.println("\t" + valueA + " % " + valueB + " = " + remainder);
+        resultKilogram =  inputMiles * ML2KL;
+
+        System.out.print    ("Converted: " + inputMiles + " kg" + " = ");
+        System.out.println  (resultKilogram + " kilogram");
+
+
+
+
+
+
     }
 }
