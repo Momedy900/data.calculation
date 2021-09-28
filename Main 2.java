b673b68e586fd99c6cfbe5d761c363ecba7379f3
Mohamed Kaba <39755459+Momedy900@users.noreply.github.com>
2021-09-28 14:45:11 -0700

    Add files via upload

---
+++ Main 2.java
@@ -0,0 +1,33 @@
+package com.mohamedkaba;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Name - Mohamed Kaba
+        // Student ID - T00690092
+        // Purpose of Program - Calculation in Java
+        // I did encounter some precision errors
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Please enter two values");
+
+        int valueA, valueB = 0;
+        System.out.print("Value A: ");
+        valueA = scan.nextInt();
+        System.out.print("Value B: ");
+        valueB = scan.nextInt();
+
+        int sum = valueA + valueB;
+        int difference = valueA - valueB;
+        int multiplication = valueA * valueB;
+        int quotient = valueA / valueB;
+        int remainder = valueA % valueB;
+
+        System.out.println("Calculation for " + valueA + " and " + valueB);
+        System.out.println("\t" + valueA + " + " + valueB + " = " + sum);
+        System.out.println("\t" + valueA + " - " + valueB + " = " + difference);
+        System.out.println("\t" + valueA + " * " + valueB + " = " + multiplication);
+        System.out.println("\t" + valueA + " / " + valueB + " = " + quotient);
+        System.out.println("\t" + valueA + " % " + valueB + " = " + remainder);
+    }
+}
